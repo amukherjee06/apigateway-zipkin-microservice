55ef61a7fe4d81a237a2e243ebc1f4d4f7eee481
Arnab Mukherjee <mukherjeearnab06@gmail.com>
2022-01-17 15:50:50 +0530

    01.Department Service initial checkin

---
+++ department-service/src/test/java/com/arnab/microservice/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnab.microservice.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
